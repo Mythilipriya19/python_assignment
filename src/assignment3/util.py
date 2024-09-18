cd6f18cc8f1a7d7ea6de84998ff6b8c6f55a3699
mythilipriya <mythilipriya.e@diggibyte.com>
2024-09-18 16:53:35 +0530

    Convert immutable string to mutable for character modification

---
+++ src/assignment3/util.py
@@ -0,0 +1,14 @@
+""" We have seen that lists are mutable (they can be changed),
+and tuples are immutable (they cannot be changed).
+Let's try to understand this with an example.
+You are given an immutable string, and you want to make changes to it."""
+
+def mutation():
+    string="Apple"
+    n=list(string)
+
+    n[2]='l'
+
+    new_str=''.join(n)
+
+    print(new_str)
