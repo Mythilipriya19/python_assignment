a8803ef230ccbd19c20f3ad38e68022a522f8d9c
mythilipriya <mythilipriya.e@diggibyte.com>
2024-09-23 17:09:50 +0530

    Calendar Module

--- src/assignment5/driver.py
+++ src/assignment5/driver.py
@@ -1,3 +1,3 @@
 from python_assignment.src.assignment5.util import datetime
 
-datetime()
+print(datetime("19 05 2002"))
--- src/assignment5/util.py
+++ src/assignment5/util.py
@@ -1,8 +1,7 @@
-def datetime():
+#
+def datetime(date):
  from datetime import datetime
- date=input("Enter Date: ")
  date_str=datetime.strptime(date, "%d %m %Y")
  day=date_str.strftime("%A")
  return day
 
-print(datetime())
