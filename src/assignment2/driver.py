61a462e147d730118dee57a7c6a884f36a7422bd
mythilipriya <mythilipriya.e@diggibyte.com>
2024-09-23 16:59:30 +0530

    Find the Runner-Up Score

--- src/assignment2/driver.py
+++ src/assignment2/driver.py
@@ -1,3 +1,3 @@
 from python_assignment.src.assignment2.util import highest
 
-highest()
+print(highest())
--- src/assignment2/util.py
+++ src/assignment2/util.py
@@ -1,6 +1,4 @@
-""" Given the participants' score sheet for your University Sports Day,
-# you are required to find the runner-up score. You are given  scores.
-# Store them in a list and find the score of the runner-up."""
+
 def highest():
     score=[50, 20, 80,90]
     first=score[0]
@@ -10,5 +8,3 @@ def highest():
             runner=first
             first=i
     return runner
-v=highest()
-print(v)
