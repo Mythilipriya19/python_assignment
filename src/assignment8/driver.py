bd5f32df99008a49d7a0fe5c79ca24f6acdc97f5
mythilipriya <mythilipriya.e@diggibyte.com>
2024-09-23 17:15:11 +0530

    Floor, Ceil and Rint

---
+++ src/assignment8/driver.py
@@ -0,0 +1,3 @@
+from python_assignment.src.assignment8.util import float_format
+
+print(float_format())
---
+++ src/assignment8/util.py
@@ -0,0 +1,9 @@
+#Floor, Ceil and Rint
+import numpy as np
+
+def float_format(n):
+
+    A=np.array(n.split(','),dtype=float)
+
+
+    return np.ceil(A) , np.floor(A) , np.rint(A)
