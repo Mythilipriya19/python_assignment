6c2087e957dd28ffab0bde4ccd344170d56b7391
mythilipriya <mythilipriya.e@diggibyte.com>
2024-09-23 17:00:14 +0530

    Mutations

--- src/assignment3/driver.py
+++ src/assignment3/driver.py
@@ -1,3 +1,3 @@
 import util
 
-util.mutation()
+print(util.mutation())
--- src/assignment3/util.py
+++ src/assignment3/util.py
@@ -1,7 +1,4 @@
-""" We have seen that lists are mutable (they can be changed),
-and tuples are immutable (they cannot be changed).
-Let's try to understand this with an example.
-You are given an immutable string, and you want to make changes to it."""
+
 
 def mutation():
     string="Apple"
@@ -12,5 +9,3 @@ def mutation():
     new_str=''.join(n)
 
     return new_str
-v=mutation()
-print(v)
