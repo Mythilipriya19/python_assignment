9eafbf3c5185a2c5eddd4977616fe035003087b4
mythilipriya <mythilipriya.e@diggibyte.com>
2024-09-23 17:48:50 +0530

    testcase for assignment12

---
+++ test/assignment12.py
@@ -0,0 +1,10 @@
+import unittest
+
+from python_assignment.src.assignment12.util import subset
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(subset(), 1)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
